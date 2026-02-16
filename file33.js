056cdb71b938b416a90ae5695200a330f46b2242
Karnam Sai Vaishnav Kumar <saivaishnav@gmail.com>
2026-02-16 11:06:27 +0530

    Added new JS files and updated file32 & file33

--- file33.js
+++ file33.js
@@ -1,6 +1,49 @@
-//const student ={
-// name:"jonh",
-// age: 21
-// }
 
-name = ["John", "Doe", "Smith"]
+// const student = {
+//   name: "john",
+//   age: 21,
+//   city:"Jalandhar"
+// };
+
+// console.log(student.name);
+
+// const { name, age } = student;
+
+// console.log(name);
+// console.log(age);
+
+// const{n1,n2}=student
+// console.log(n1);
+// console.log(n2);
+
+// const student = {
+//   name: "john",
+//   age: 21,
+//   city:"Jalandhar"
+// };
+// const{city="Amritsar" } = student
+// console.log(city);
+
+const student = {
+  name: "john",
+  age: 21,
+  city:"Jalandhar",
+  marks:{
+    math:60,
+    science:990
+  }
+};
+const {math,science} = student.marks
+console.log(math);
+console.log(science);
+
+
+
+
+
+
+
+
+
+
+
---
+++ file34.js
@@ -0,0 +1,11 @@
+function response(user,email,role){
+    console.log(user)
+    console.log(email)
+}
+const user = {
+    name:"john",
+    age:21,
+    email:"john@gmail.com",
+    role:"user"
+};
+response(user)
